7a0d8b925645e37ab76766a86e8353e6ee3ee056
Ali Husnain <139127727+alihusnain010203@users.noreply.github.com>
2024-09-21 12:02:58 +0500

    exercise with Go functions

--- essentials/app.go
+++ essentials/app.go
@@ -5,26 +5,23 @@ import (
 )
 
 func main() {
-	var expenses, revenue, tax int
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter tax: ")
-	fmt.Scan(&tax)
+	// var  tax int
+	// fmt.Print("Enter expenses: ")
+	// fmt.Scan(&expenses)
+	expenses := getUserInfo("expenses")
+	revenue := getUserInfo("revenue")
+	tax := getUserInfo("Tax")
 
-	earningBeforeTax := revenue - expenses
-	earningAfterTax := earningBeforeTax - tax
-
-    ratio:=earningBeforeTax/earningAfterTax;
-    // Display output in one line
+	// Display output in one line
 	// fmt.Printf(`Earning before tax: %d,
 	//  Earning after tax: %d,
 	//   Ratio: %v`, earningBeforeTax, earningAfterTax, ratio)
-	
+
 	// Storing string in variable
-    RatioWithString:=fmt.Sprintf("Ratio is: %v",ratio)
-    fmt.Printf("Earning before tax: %d\n, Earning after tax: %d\n, Ratio: %v\n", earningBeforeTax, earningAfterTax, ratio)
+	earningBeforeTax, earningAfterTax, ratio := performCalculation(revenue, expenses, tax)
+
+	RatioWithString := fmt.Sprintf("Ratio is: %v", ratio)
+	fmt.Printf("Earning before tax: %.2f\n, Earning after tax: %.2f\n, Ratio: %.2f\n", earningBeforeTax, earningAfterTax, ratio)
 	fmt.Print(RatioWithString)
 	sum := add(1, 2)
 	fmt.Printf("\nSum: %d", sum)
@@ -33,3 +30,18 @@ func main() {
 func add(a int, b int) int {
 	return a + b
 }
+
+func getUserInfo(text string) (inputVal float64) {
+	var InputVal float64
+	fmt.Print("Enter value of ", text)
+	fmt.Scan(&InputVal)
+	return InputVal
+}
+func performCalculation(revenue, expenses, tax float64) (earningBeforeTax, earningAfterTax, ratio float64) {
+	earningBeforeTax = revenue - expenses
+	earningAfterTax = earningBeforeTax - tax
+
+	ratio = earningBeforeTax / earningAfterTax
+	return earningBeforeTax, earningAfterTax, ratio
+}
+
