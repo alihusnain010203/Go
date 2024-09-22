991ec47fc5aae53d68db471951e81b843208124b
Ali Husnain <139127727+alihusnain010203@users.noreply.github.com>
2024-09-22 13:18:08 +0500

    refactor code

--- essentials/ControlStructure/bank.go
+++ essentials/ControlStructure/bank.go
@@ -1,75 +1,75 @@
-package main
+	package main
 
-import (
-	"fmt"
-	"github.com/brianvoe/gofakeit/v7"
-	"examp.co/bank/fileops"
-)
+	import (
+		"fmt"
+		"github.com/brianvoe/gofakeit/v7"
+		"examp.co/bank/fileops"
+	)
 
-const accountBalanceFile = "balance.txt"
+	const accountBalanceFile = "balance.txt"
 
-func main() {
-	var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile)
+	func main() {
+		var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile)
 
-	if err != nil {
-		fmt.Println("ERROR")
-		fmt.Println(err)
-		fmt.Println("---------")
-		// panic("Can't continue, sorry.")
-	}
+		if err != nil {
+			fmt.Println("ERROR")
+			fmt.Println(err)
+			fmt.Println("---------")
+			// panic("Can't continue, sorry.")
+		}
 
-	fmt.Println("Welcome to Go Bank!")
+		fmt.Println("Welcome to Go Bank!")
 
-	for {
+		for {
 
-		var choice int
-		presentOptions()
-		fmt.Print("Your choice: ")
-		fmt.Scan(&choice)
-		fmt.Println(gofakeit.Phone());
+			var choice int
+			presentOptions()
+			fmt.Print("Your choice: ")
+			fmt.Scan(&choice)
+			fmt.Println(gofakeit.Phone());
 
-		// wantsCheckBalance := choice == 1
+			// wantsCheckBalance := choice == 1
 
-		switch choice {
-		case 1:
-			fmt.Println("Your balance is", accountBalance)
-		case 2:
-			fmt.Print("Your deposit: ")
-			var depositAmount float64
-			fmt.Scan(&depositAmount)
+			switch choice {
+			case 1:
+				fmt.Println("Your balance is", accountBalance)
+			case 2:
+				fmt.Print("Your deposit: ")
+				var depositAmount float64
+				fmt.Scan(&depositAmount)
 
-			if depositAmount <= 0 {
-				fmt.Println("Invalid amount. Must be greater than 0.")
-				// return
-				continue
+				if depositAmount <= 0 {
+					fmt.Println("Invalid amount. Must be greater than 0.")
+					// return
+					continue
+				}
+
+				accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+				fmt.Println("Balance updated! New amount:", accountBalance)
+				fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
+			case 3:
+				fmt.Print("Withdrawal amount: ")
+				var withdrawalAmount float64
+				fmt.Scan(&withdrawalAmount)
+
+				if withdrawalAmount <= 0 {
+					fmt.Println("Invalid amount. Must be greater than 0.")
+					continue
+				}
+
+				if withdrawalAmount > accountBalance {
+					fmt.Println("Invalid amount. You can't withdraw more than you have.")
+					continue
+				}
+
+				accountBalance -= withdrawalAmount // accountBalance = accountBalance + depositAmount
+				fmt.Println("Balance updated! New amount:", accountBalance)
+				fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
+			default:
+				fmt.Println("Goodbye!")
+				fmt.Println("Thanks for choosing our bank")
+				return
+				// break
 			}
-
-			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
-			fmt.Println("Balance updated! New amount:", accountBalance)
-			fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
-		case 3:
-			fmt.Print("Withdrawal amount: ")
-			var withdrawalAmount float64
-			fmt.Scan(&withdrawalAmount)
-
-			if withdrawalAmount <= 0 {
-				fmt.Println("Invalid amount. Must be greater than 0.")
-				continue
-			}
-
-			if withdrawalAmount > accountBalance {
-				fmt.Println("Invalid amount. You can't withdraw more than you have.")
-				continue
-			}
-
-			accountBalance -= withdrawalAmount // accountBalance = accountBalance + depositAmount
-			fmt.Println("Balance updated! New amount:", accountBalance)
-			fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
-		default:
-			fmt.Println("Goodbye!")
-			fmt.Println("Thanks for choosing our bank")
-			return
-			// break
 		}
 	}
-}
