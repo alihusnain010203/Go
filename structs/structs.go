8c8844210b5b286d3ea46cfd4e3511e6716d29f8
Ali Husnain <139127727+alihusnain010203@users.noreply.github.com>
2024-09-24 09:37:46 +0500

    packages and Inheritance

--- structs/structs.go
+++ structs/structs.go
@@ -2,54 +2,52 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"example.com/structs/user"
 )
 
-type User struct {
-	firstName string
-	lastName  string
-	birthday  string
-	createdAt time.Time
-}
-func (user *User) OutputStruct() {
-	fmt.Println(user.firstName, user.lastName, user.birthday,user.createdAt)
-	// use de refernce
-	fmt.Println((*user).firstName, (*user).lastName, (*user).birthday,(*user).createdAt)
-
-}
 
 func main() {
 	userfirstName := getUserInput("Enter Your Firstname.!!!")
 	userlastName := getUserInput("Enter Your Lastname!!!")
 	userbirthday := getUserInput("Enter Your Date Of Birth DD/MM/YYYY!!!")
 
-	var Appuser User
-	Appuser = User{
-		firstName: userfirstName,
-		lastName:  userlastName,
-		birthday:  userbirthday,
-		createdAt: time.Now(),
+	var Appuser *user.User
+	// Appuser = User{
+	// 	firstName: userfirstName,
+	// 	lastName:  userlastName,
+	// 	birthday:  userbirthday,
+	// 	createdAt: time.Now(),
+	// }
+
+	Appuser,err :=user.New(userfirstName,userlastName,userbirthday)
+	if(err!=nil){
+		fmt.Println(err)
+		panic(err)
 	}
+	
+	// Output(&Appuser)
 
-	Output(&Appuser)
-
-	Appuser.OutputStruct();
+	Appuser.OutputStruct()
+	Appuser.Clear()
+	Appuser.OutputStruct()
 
 }
+
 // get Value of Struct and the copy og user created in memory
-// func Output(user User) {
-// 	fmt.Println(user.firstName, user.lastName, user.birthday,user.createdAt)
-// }
-func Output(user *User) {
-	fmt.Println(user.firstName, user.lastName, user.birthday,user.createdAt)
-	// use de refernce
-	fmt.Println((*user).firstName, (*user).lastName, (*user).birthday,(*user).createdAt)
+//
+//	func Output(user User) {
+//		fmt.Println(user.firstName, user.lastName, user.birthday,user.createdAt)
+//	}
+// func Output(user *User) {
+// 	fmt.Println(user.firstName, user.lastName, user.birthday, user.createdAt)
+// 	// use de refernce
+// 	fmt.Println((*user).firstName, (*user).lastName, (*user).birthday, (*user).createdAt)
 
-}
+// }
 func getUserInput(prompt string) string {
 	fmt.Println(prompt)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 
 }
