1c847b8e75a7627e1ff204e72f01ae39d6258056
Ali Husnain <139127727+alihusnain010203@users.noreply.github.com>
2024-10-06 12:56:26 +0500

    Update Pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,3 +1,30 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// var a int = 20
+// 	// ip := &a
+
+// 	// fmt.Println("Address of a variable: ", &a)
+// 	// fmt.Println("Address stored in ip variable: ", ip)
+// 	// fmt.Println("Value of *ip variable: ", *ip)
+// 	age := 32
+// 	//    var ageP *int
+// 	//    ageP=&age
+// 	ageP := &age
+// 	fmt.Println("Age :", *ageP)
+// 	fmt.Println("Age Adress:" ,ageP)
+// 	getAdultAge(ageP)
+//    	fmt.Println("Age :", *ageP)
+// 	fmt.Println("Age :" ,age)
+// }
+
+// func getAdultAge(age *int)  {
+// 	fmt.Print(age)
+//     *age=*age-18;
+// 	// return *age + 18
+// }
 package main
 
 import "fmt"
