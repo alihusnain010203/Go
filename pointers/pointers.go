5f4bb0648f724db4760dd358b4999500c1b7d80b
Ali Husnain <139127727+alihusnain010203@users.noreply.github.com>
2024-10-25 10:09:13 +0500

    update

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,3 +1,30 @@
+// // package main
+
+// // import "fmt"
+
+// // func main() {
+// // 	// var a int = 20
+// // 	// ip := &a
+
+// // 	// fmt.Println("Address of a variable: ", &a)
+// // 	// fmt.Println("Address stored in ip variable: ", ip)
+// // 	// fmt.Println("Value of *ip variable: ", *ip)
+// // 	age := 32
+// // 	//    var ageP *int
+// // 	//    ageP=&age
+// // 	ageP := &age
+// // 	fmt.Println("Age :", *ageP)
+// // 	fmt.Println("Age Adress:" ,ageP)
+// // 	getAdultAge(ageP)
+// //    	fmt.Println("Age :", *ageP)
+// // 	fmt.Println("Age :" ,age)
+// // }
+
+// // func getAdultAge(age *int)  {
+// // 	fmt.Print(age)
+// //     *age=*age-18;
+// // 	// return *age + 18
+// // }
 // package main
 
 // import "fmt"
@@ -25,30 +52,58 @@
 //     *age=*age-18;
 // 	// return *age + 18
 // }
-package main
 
-import "fmt"
+// // package main
 
-func main() {
-	// var a int = 20
-	// ip := &a
+// // import "fmt"
 
-	// fmt.Println("Address of a variable: ", &a)
-	// fmt.Println("Address stored in ip variable: ", ip)
-	// fmt.Println("Value of *ip variable: ", *ip)
-	age := 32
-	//    var ageP *int
-	//    ageP=&age
-	ageP := &age
-	fmt.Println("Age :", *ageP)
-	fmt.Println("Age Adress:" ,ageP)
-	getAdultAge(ageP)
-   	fmt.Println("Age :", *ageP)
-	fmt.Println("Age :" ,age)
-}
+// // func main() {
+// // 	// var a int = 20
+// // 	// ip := &a
 
-func getAdultAge(age *int)  {
-	fmt.Print(age)
-    *age=*age-18;
-	// return *age + 18
-}
+// // 	// fmt.Println("Address of a variable: ", &a)
+// // 	// fmt.Println("Address stored in ip variable: ", ip)
+// // 	// fmt.Println("Value of *ip variable: ", *ip)
+// // 	age := 32
+// // 	//    var ageP *int
+// // 	//    ageP=&age
+// // 	ageP := &age
+// // 	fmt.Println("Age :", *ageP)
+// // 	fmt.Println("Age Adress:" ,ageP)
+// // 	getAdultAge(ageP)
+// //    	fmt.Println("Age :", *ageP)
+// // 	fmt.Println("Age :" ,age)
+// // }
+
+// // func getAdultAge(age *int)  {
+// // 	fmt.Print(age)
+// //     *age=*age-18;
+// // 	// return *age + 18
+// // }
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// var a int = 20
+// 	// ip := &a
+
+// 	// fmt.Println("Address of a variable: ", &a)
+// 	// fmt.Println("Address stored in ip variable: ", ip)
+// 	// fmt.Println("Value of *ip variable: ", *ip)
+// 	age := 32
+// 	//    var ageP *int
+// 	//    ageP=&age
+// 	ageP := &age
+// 	fmt.Println("Age :", *ageP)
+// 	fmt.Println("Age Adress:" ,ageP)
+// 	getAdultAge(ageP)
+//    	fmt.Println("Age :", *ageP)
+// 	fmt.Println("Age :" ,age)
+// }
+
+// func getAdultAge(age *int)  {
+// 	fmt.Print(age)
+//     *age=*age-18;
+// 	// return *age + 18
+// }
